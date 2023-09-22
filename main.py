f492f9fc98dcc4b83e696bf4f8bddc7d11420546
Suryaca <145754685+Suryaca@users.noreply.github.com>
2023-09-22 13:47:04 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-def fact_rec(n):
-   if n==0 or n==1:
-      return 1
-   else:
-      return n*fact_rec(n-1)
-number=int(input ("enter a value:"))
-res=fact_rec(number)
-print ("the factorial of {}is{}.".format (number,res))
+class BankAccount:
+
+  def __init__(self, account_number, account_holder_name, initial_balance):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balance
+
+  def deposit(self, amount):
+    self.__account_balance += amount
+
+  def withdraw(self, amount):
+    if amount <= self.__account_balance:
+      self.__account_balance -= amount
+    else:
+      print("Insufficient funds")
+
+  def display_balance(self):
+    return self.__account_balance
+
+
+# Testing the BankAccount class
+account = BankAccount("123456789", "John Doe", 1000)
+
+# Deposit money
+account.deposit(500)
+
+# Display account balance
+print(f"Current balance: {account.display_balance()}")
+
+# Withdraw money
+account.withdraw(200)
+
+# Display updated balance
+print(f"Updated balance: {account.display_balance()}")
